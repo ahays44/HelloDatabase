eae8edca725bb1ac2b6080199f524239e7591cc3
austinhays <ahays.target@yahoo.com>
2016-06-13 11:41:09 -0400

    Initial Import

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,39 @@
+package com.theironyard;
+
+import org.h2.tools.Server;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS players (id IDENTITY, name VARCHAR, is_alive BOOLEAN, score INT, health DOUBLE)");
+        stmt.execute("INSERT INTO players VALUES (null, 'Alice', TRUE, 0, 100)");
+        stmt.execute("DELETE FROM players WHERE name = 'Alice'");
+
+        // BAD!!!!!
+        // String name = "Charlie";
+        // stmt.execute(String.format("INSERT INTO players VALUES (NULL, '%s', TRUE, 0, 100)", name));
+
+        //GOOD
+        String name = "Charlie";
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (NULL, ?, ?, 0, 100)");
+        stmt2.setString(1, name);
+        stmt2.setBoolean(2, true);
+        stmt2.execute();
+
+        PreparedStatement stmt3 = conn.prepareStatement("SELECT * FROM players");
+        ResultSet results = stmt3.executeQuery();
+        while (results.next()) {
+            int id = results.getInt("id");
+            String name2 = results.getString("name");
+            boolean isAlive = results.getBoolean("is_alive");
+            System.out.println(id + " " + name2 + " " + isAlive);
+        }
+    }
+}
